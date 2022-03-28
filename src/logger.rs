f35ad511060bd6946bddc803f0ea1fc0b126a189
Borovkov Sergey <707grays@gmail.com>
2022-03-28 10:42:05 +0300

    moved logger Enhancement

--- src/logger.rs
+++ src/logger.rs
@@ -1,19 +1,28 @@
-use std::{path::PathBuf, str::FromStr};
+use std::path::PathBuf;
 
 use anyhow::Result;
 use chrono::Local;
-use flexi_logger::{
-    Age, Cleanup, Criterion, Duplicate, FileSpec, Level, Logger, Naming, WriteMode,
-};
-use serde::{de::Error, Deserialize, Deserializer};
+use flexi_logger::{Age, Cleanup, Criterion, Duplicate, FileSpec, Logger, Naming, WriteMode};
+use serde::Deserialize;
 
 pub fn init_logger(logger_settings: LoggerSettings) -> Result<()> {
-    let file_spec = FileSpec::try_from(logger_settings.path.clone())?;
+    let mut logger = Logger::try_with_str(&logger_settings.spec)?;
 
-    let _ = Logger::try_with_str(logger_settings.level.as_str())?
-        .log_to_file(file_spec.clone())
-        .write_mode(WriteMode::Direct) // if high performance mode is required need to use WriteMode::BufferAndFlush
-        .duplicate_to_stdout(Duplicate::Info)
+    if let Some(path) = &logger_settings.path {
+        let file_spec = FileSpec::try_from(path.clone())?;
+        logger = logger
+            .log_to_file(file_spec)
+            .duplicate_to_stdout(Duplicate::All)
+            .write_mode(WriteMode::Direct) // if high performance mode is required need to use WriteMode::BufferAndFlush
+            .rotate(
+                Criterion::Age(Age::Day),
+                Naming::Timestamps,
+                Cleanup::KeepLogFiles(logger_settings.keep_log_for_days),
+            )
+            .append()
+    };
+
+    logger
         .use_utc()
         .format(|out, _, record| {
             out.write_fmt(format_args!(
@@ -24,44 +33,43 @@ pub fn init_logger(logger_settings: LoggerSettings) -> Result<()> {
                 &record.args()
             ))
         })
-        .rotate(
-            Criterion::Age(Age::Day),
-            Naming::Timestamps,
-            Cleanup::KeepLogFiles(logger_settings.keep_log_for_days),
-        )
-        .append()
         .start()?;
 
-    log::info!(
-        "Logger has been successfully initialized. All logs will be stored in the file: {}",
-        logger_settings.path.display()
-    );
+    log::info!("Logger has been successfully initialized.");
+    if let Some(path) = &logger_settings.path {
+        log::info!("All logs will be stored in the file: {}", path.display());
+    }
+
     Ok(())
 }
 
 #[derive(Debug, Deserialize, Eq, PartialEq)]
 #[serde(default)]
 pub struct LoggerSettings {
-    #[serde(deserialize_with = "deserialize_log_level")]
-    pub level: Level,
-    pub path: PathBuf,
-    pub keep_log_for_days: usize,
-}
+    #[serde(default = "default_spec")]
+    pub spec: String,
 
-fn deserialize_log_level<'de, D>(deserializer: D) -> Result<Level, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let level = String::deserialize(deserializer)?;
-    Level::from_str(&level).map_err(Error::custom)
+    #[serde(default)]
+    pub path: Option<PathBuf>,
+
+    #[serde(default = "default_keep_log_for_days")]
+    pub keep_log_for_days: usize,
 }
 
 impl Default for LoggerSettings {
     fn default() -> Self {
         Self {
-            level: Level::Debug,
-            path: PathBuf::default().join("log.txt"),
-            keep_log_for_days: 7,
+            spec: default_spec(),
+            path: None,
+            keep_log_for_days: default_keep_log_for_days(),
         }
     }
+}
+
+fn default_spec() -> String {
+    "info".into()
+}
+
+const fn default_keep_log_for_days() -> usize {
+    7
 }
