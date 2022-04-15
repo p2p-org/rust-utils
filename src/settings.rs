1293c3eb72fa6808fa9d57d3c1f8bcc161df85fd
Konstantin Stepanov <me@kstep.me>
2022-04-15 18:08:57 +0400

    settings helpers

---
+++ src/settings.rs
@@ -0,0 +1,51 @@
+use config::{Config, ConfigError};
+use serde::de::DeserializeOwned;
+
+pub static DEFAULT_SETTINGS_FILE: &str = "settings.toml";
+
+pub fn get_settings_file() -> String {
+    std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| DEFAULT_SETTINGS_FILE.to_owned())
+}
+
+pub fn try_read_config<T, E>(env_prefix: &str) -> Result<T, E>
+where
+    T: DeserializeOwned,
+    E: From<ConfigError>,
+{
+    let file = get_settings_file();
+    try_read_file_config(&file, env_prefix)
+}
+
+pub fn try_read_file_config<T, E>(file: &str, env_prefix: &str) -> Result<T, E>
+where
+    T: DeserializeOwned,
+    E: From<ConfigError>,
+{
+    Config::builder()
+        .add_source(config::File::with_name(file).required(false))
+        .add_source(config::Environment::with_prefix(env_prefix).separator("__"))
+        .build()
+        .and_then(Config::try_deserialize)
+        .map_err(Into::into)
+}
+
+pub fn read_config_or_default<T>(env_prefix: &str) -> T
+where
+    T: DeserializeOwned + Default,
+{
+    let file = get_settings_file();
+    read_file_config_or_default(&file, env_prefix)
+}
+
+pub fn read_file_config_or_default<T>(file: &str, env_prefix: &str) -> T
+where
+    T: DeserializeOwned + Default,
+{
+    try_read_file_config::<T, ConfigError>(file, env_prefix)
+        .map_err(|error| {
+            log::warn!("config error: {error}, going on with default config...");
+        })
+        .unwrap_or_default()
+}
