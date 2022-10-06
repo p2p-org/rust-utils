5452e3d9ed4bf232669f990af3c0f0a7231900e2
Konstantin Stepanov <me@kstep.me>
2022-10-06 21:28:36 +0300

    add support for solana sdk keypair

--- src/crypto.rs
+++ src/crypto.rs
@@ -39,6 +39,44 @@ impl PublicKeyExt for PublicKey {
     }
 }
 
+#[cfg(feature = "solana-sdk")]
+mod solana {
+    use super::{KeypairExt, PublicKeyExt};
+    use borsh::BorshSerialize;
+    use ed25519_dalek::{PublicKey, Signature, SignatureError};
+    use solana_sdk::{
+        pubkey::Pubkey,
+        signer::{keypair::Keypair, Signer},
+    };
+
+    impl KeypairExt for Keypair {
+        fn new_rand() -> Self {
+            Keypair::new()
+        }
+
+        fn sign_borsh<M: BorshSerialize>(&self, message: &M) -> Signature {
+            let message = borsh::to_vec(message).expect("message must be serializable");
+            let signature = self.sign_message(&message);
+            Signature::from_bytes(signature.as_ref()).expect("invalid signature")
+        }
+    }
+
+    impl PublicKeyExt for Pubkey {
+        fn new_zeroed() -> Self {
+            Pubkey::default()
+        }
+
+        fn to_base58(&self) -> String {
+            self.to_string()
+        }
+
+        fn verify_borsh<M: BorshSerialize>(&self, message: &M, signature: &Signature) -> Result<(), SignatureError> {
+            let pubkey = PublicKey::from_bytes(self.as_ref())?;
+            pubkey.verify_borsh(message, signature)
+        }
+    }
+}
+
 #[cfg(all(test))]
 mod tests {
     use super::*;
@@ -65,5 +103,33 @@ mod tests {
         let second_keypair = Keypair::new_rand();
 
         assert!(second_keypair.public.verify_borsh(&message, &signature).is_err());
+    }
+
+    #[cfg(feature = "solana-sdk")]
+    #[test]
+    fn check_solana_signing() {
+        use solana_sdk::signer::{keypair::Keypair, Signer};
+        let keypair = Keypair::new_rand();
+
+        let message = "Hello world".to_string();
+
+        let signature = keypair.sign_borsh(&message);
+
+        assert!(keypair.pubkey().verify_borsh(&message, &signature).is_ok());
+    }
+
+    #[cfg(feature = "solana-sdk")]
+    #[test]
+    fn check_solana_signing_wrong_pubkey() {
+        use solana_sdk::signer::{keypair::Keypair, Signer};
+        let keypair = Keypair::new_rand();
+
+        let message = "Hello world".to_string();
+
+        let signature = keypair.sign_borsh(&message);
+
+        let second_keypair = Keypair::new_rand();
+
+        assert!(second_keypair.pubkey().verify_borsh(&message, &signature).is_err());
     }
 }
