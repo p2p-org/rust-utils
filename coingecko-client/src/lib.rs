c2aee6d76cfe188b0dc1428c0d9dd0742a000367
Borovkov Sergey <707grays@gmail.com>
2023-08-08 15:14:43 +0300

    coingecko-client moved from token-service

---
+++ coingecko-client/src/lib.rs
@@ -0,0 +1,127 @@
+use anyhow::Context;
+use http::{HeaderMap, HeaderName, StatusCode};
+use http_client::settings::HttpClientSettings;
+use serde::Deserialize;
+use serde_json::Value;
+use std::collections::HashMap;
+use token_address::StoredTokenAddress;
+use types::{CoingeckoInfo, CoingeckoInfoWithAddress};
+
+pub mod types;
+
+const PUBLIC_BASE_URL: &str = "https://api.coingecko.com/api/v3";
+const PRO_BASE_URL: &str = "https://pro-api.coingecko.com/api/v3";
+
+pub struct CoingeckoClient {
+    client: reqwest::Client,
+    base_url: String,
+}
+
+impl CoingeckoClient {
+    pub fn new(settings: HttpClientSettings) -> anyhow::Result<Self> {
+        let HttpClientSettings {
+            tcp_keepalive,
+            pool_idle_timeout,
+            api_key,
+            ..
+        } = settings;
+
+        let base_url = if api_key.is_some() {
+            PRO_BASE_URL
+        } else {
+            PUBLIC_BASE_URL
+        };
+
+        let mut builder = reqwest::ClientBuilder::new()
+            .tcp_keepalive(Some(tcp_keepalive))
+            .pool_idle_timeout(Some(pool_idle_timeout));
+
+        if let Some(api_key) = api_key {
+            builder = builder.default_headers(HeaderMap::from_iter([(
+                HeaderName::from_static("x-cg-pro-api-key"),
+                api_key.try_into()?,
+            )]));
+        };
+
+        let client = builder.build().context("Unable to build coingecko client")?;
+
+        Ok(Self {
+            client,
+            base_url: base_url.to_string(),
+        })
+    }
+
+    pub async fn get_metadata_by_address(&self, address: &StoredTokenAddress) -> anyhow::Result<Option<Value>> {
+        let response = self
+            .client
+            .get(format!(
+                "{base_url}/coins/{platform}/contract/{address}",
+                base_url = self.base_url,
+                platform = address.platform(),
+            ))
+            .send()
+            .await?;
+
+        if response.status() == StatusCode::NOT_FOUND {
+            return Ok(None);
+        }
+
+        let response = response.error_for_status()?;
+
+        Ok(Some(response.json::<Value>().await?.into()))
+    }
+
+    pub async fn get_metadata_by_slug(&self, slug: &str) -> anyhow::Result<Option<CoingeckoInfoWithAddress>> {
+        let response = self
+            .client
+            .get(format!("{base_url}/coins/{slug}", base_url = self.base_url))
+            .send()
+            .await?;
+
+        if response.status() == StatusCode::NOT_FOUND {
+            return Ok(None);
+        }
+
+        let response = response.error_for_status()?;
+
+        Ok(Some(response.json::<CoingeckoCoinsResponse>().await?.into()))
+    }
+
+    pub async fn get_all_metadata(&self) -> anyhow::Result<HashMap<String, CoingeckoInfoWithAddress>> {
+        let response = self
+            .client
+            .get(format!("{base_url}/coins/list", base_url = self.base_url))
+            .send()
+            .await?;
+
+        let response = response.error_for_status()?;
+
+        Ok(response
+            .json::<Vec<CoingeckoCoinsResponse>>()
+            .await?
+            .into_iter()
+            .map(|v| (v.id.clone(), v.into()))
+            .collect())
+    }
+}
+
+#[derive(Deserialize)]
+struct CoingeckoCoinsResponse {
+    id: String,
+    symbol: String,
+    name: String,
+    platforms: HashMap<String, String>,
+}
+
+impl From<CoingeckoCoinsResponse> for CoingeckoInfoWithAddress {
+    fn from(value: CoingeckoCoinsResponse) -> Self {
+        Self {
+            metadata: CoingeckoInfo {
+                coin_id: value.id,
+                symbol: value.symbol,
+                name: value.name,
+            },
+            addresses: value.platforms,
+        }
+    }
+}
---
+++ coingecko-client/src/types.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
+
+#[derive(Debug, FromRow, Serialize, Deserialize, Clone, PartialEq, Eq, Hash)]
+pub struct CoingeckoInfo {
+    pub coin_id: String,
+    pub name: String,
+    pub symbol: String,
+}
+
+#[derive(PartialEq, Eq, Clone)]
+pub struct CoingeckoInfoWithAddress {
+    pub metadata: CoingeckoInfo,
+    pub addresses: HashMap<String, String>, // Platform, address
+}
